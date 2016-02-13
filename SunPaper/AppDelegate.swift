328d6ddea14de2d15f5ddd67f5f7209d150e1d71
David Celis <me@davidcel.is>
2016-02-13 13:28:48 -0800

    Initial Commit

---
+++ SunPaper/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SunPaper
+//
+//  Created by David Celis on 2/13/16.
+//  Copyright © 2016 David Celis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
