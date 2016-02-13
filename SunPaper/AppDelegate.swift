891b7edf65892168105191441e44265a9851a30c
David Celis <me@davidcel.is>
2016-02-13 13:40:41 -0800

    Add menu bar item with Quit functionality
    Signed-off-by: David Celis <me@davidcel.is>

--- SunPaper/AppDelegate.swift
+++ SunPaper/AppDelegate.swift
@@ -11,11 +11,17 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var statusMenu: NSMenu!
 
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
+
+    @IBAction func quitClicked(sender: NSMenuItem) {
+        NSApplication.sharedApplication().terminate(self)
+    }
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        statusItem.title = "SunPaper"
+        statusItem.menu = statusMenu
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
