0df5384cdeee8a8cc13a43d03be91bfb90eef290
David Celis <me@davidcel.is>
2016-02-14 17:01:12 -0800

    Write images to sandbox on drag'n'drop
    Signed-off-by: David Celis <me@davidcel.is>

--- SunPaper/PreferencesWindow.swift
+++ SunPaper/PreferencesWindow.swift
@@ -8,13 +8,19 @@
 
 import Cocoa
 
-class PreferencesWindow: NSWindowController, NSWindowDelegate {
-    
-    @IBOutlet weak var sunriseWallpaper: NSImageView!
-    @IBOutlet weak var morningWallpaper: NSImageView!
-    @IBOutlet weak var afternoonWallpaper: NSImageView!
-    @IBOutlet weak var sunsetWallpaper: NSImageView!
-    @IBOutlet weak var nightWallpaper: NSImageView!
+class PreferencesWindow: NSWindowController {
+    static let directory = NSHomeDirectory()
+    let sunrisePath = "\(directory)/sunrise.png"
+    let morningPath = "\(directory)/morning.png"
+    let afternoonPath = "\(directory)/afternoon.png"
+    let sunsetPath = "\(directory)/sunset.png"
+    let nightPath = "\(directory)/night.png"
+
+    @IBOutlet weak var sunriseImageView: NSImageView!
+    @IBOutlet weak var morningImageView: NSImageView!
+    @IBOutlet weak var afternoonImageView: NSImageView!
+    @IBOutlet weak var sunsetImageView: NSImageView!
+    @IBOutlet weak var nightImageView: NSImageView!
 
     override var windowNibName: String! {
         return "PreferencesWindow"
@@ -27,8 +33,83 @@ class PreferencesWindow: NSWindowController, NSWindowDelegate {
         self.window?.makeKeyAndOrderFront(nil)
         NSApp.activateIgnoringOtherApps(true)
     }
-    
-    func windowWillClose(notification: NSNotification) {
 
+    func loadExistingWallpapers() {
+        let fileManager = NSFileManager.defaultManager()
+
+        if fileManager.fileExistsAtPath(sunrisePath) {
+            let sunriseData = fileManager.contentsAtPath(sunrisePath)
+            sunriseImageView.image = NSImage(data: sunriseData!)
+        }
+
+        if fileManager.fileExistsAtPath(morningPath) {
+            let morningData = fileManager.contentsAtPath(morningPath)
+            morningImageView.image = NSImage(data: morningData!)
+        }
+
+        if fileManager.fileExistsAtPath(afternoonPath) {
+            let afternoonData = fileManager.contentsAtPath(afternoonPath)
+            afternoonImageView.image = NSImage(data: afternoonData!)
+        }
+
+        if fileManager.fileExistsAtPath(sunsetPath) {
+            let sunsetData = fileManager.contentsAtPath(sunsetPath)
+            sunsetImageView.image = NSImage(data: sunsetData!)
+        }
+
+        if fileManager.fileExistsAtPath(nightPath) {
+            let nightData = fileManager.contentsAtPath(nightPath)
+            nightImageView.image = NSImage(data: nightData!)
+        }
+    }
+
+    @IBAction func sunriseImageDropped(sender: NSImageView) {
+        if let image = sender.image {
+            let fileManager = NSFileManager.defaultManager()
+
+            let bmp = NSBitmapImageRep(data: image.TIFFRepresentation!)
+            let png = bmp!.representationUsingType(NSBitmapImageFileType.NSPNGFileType, properties: [:])
+            fileManager.createFileAtPath(sunrisePath, contents: png, attributes: nil)
+        }
+    }
+
+    @IBAction func morningImageDropped(sender: NSImageView) {
+        if let image = sender.image {
+            let fileManager = NSFileManager.defaultManager()
+
+            let bmp = NSBitmapImageRep(data: image.TIFFRepresentation!)
+            let png = bmp!.representationUsingType(NSBitmapImageFileType.NSPNGFileType, properties: [:])
+            fileManager.createFileAtPath(morningPath, contents: png, attributes: nil)
+        }
+    }
+
+    @IBAction func afternoonImageDropped(sender: NSImageView) {
+        if let image = sender.image {
+            let fileManager = NSFileManager.defaultManager()
+
+            let bmp = NSBitmapImageRep(data: image.TIFFRepresentation!)
+            let png = bmp!.representationUsingType(NSBitmapImageFileType.NSPNGFileType, properties: [:])
+            fileManager.createFileAtPath(afternoonPath, contents: png, attributes: nil)
+        }
+    }
+
+    @IBAction func sunsetImageDropped(sender: NSImageView) {
+        if let image = sender.image {
+            let fileManager = NSFileManager.defaultManager()
+
+            let bmp = NSBitmapImageRep(data: image.TIFFRepresentation!)
+            let png = bmp!.representationUsingType(NSBitmapImageFileType.NSPNGFileType, properties: [:])
+            fileManager.createFileAtPath(sunsetPath, contents: png, attributes: nil)
+        }
+    }
+
+    @IBAction func nightImageDropped(sender: NSImageView) {
+        if let image = sender.image {
+            let fileManager = NSFileManager.defaultManager()
+
+            let bmp = NSBitmapImageRep(data: image.TIFFRepresentation!)
+            let png = bmp!.representationUsingType(NSBitmapImageFileType.NSPNGFileType, properties: [:])
+            fileManager.createFileAtPath(nightPath, contents: png, attributes: nil)
+        }
     }
 }
